3111407638be47d88d833c1b8dda708c2f7267a6
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-03-09 13:31:29 -0600

    add task a

---
+++ test-ascii.cpp
@@ -0,0 +1,13 @@
+//Name : Irene He He
+//GitHub Name : Irene06he
+
+#include <iostream>
+
+int main() {
+  std::string a = "Cat :3 Dog";
+  for (int i = 0 ; i < a.length();i++){
+    std::cout<<a[i];
+    std::cout<<" "<<int(a[i])<<"\n";
+  }
+  return 0;
+}
