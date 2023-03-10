910bb2b3efc9520f1a1e53c5082709d178736a70
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-03-10 05:32:28 -0600

    add task b and c

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+//Name : Irene He He
+//GitHub Name : Irene06he
+
 #include <iostream>
-
-
+#include <string>
+#include "caesar.h"
+#include "vigenere.h"
 #include "funcs.h"
 
 int main()
 {
+//task b
+    std::cout<<encryptCaesar("Hello, World!", 10)<<std::endl;
+    std::cout<<encryptCaesar("Way to go!", 5)<<std::endl;
+
+// task c
+    std::cout<<encryptVigenere("Hello,World!", "cake")<<std::endl;
+    std::cout<<encryptVigenere("Way to go!", "cake")<<std::endl;
+
   return 0;
 }
