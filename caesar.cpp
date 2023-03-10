818349e2af8aa86ccc1763423b0957dd799a30d2
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-03-10 01:45:54 -0600

    add task b

---
+++ caesar.cpp
@@ -0,0 +1,31 @@
+//Name : Irene He He
+//GitHub Name : Irene06he
+
+
+#include <iostream>
+#include<string>
+#include "caesar.h"
+
+// A helper function to shift one character by rshift
+char shiftChar(char c, int rshift){
+  char value;
+  if (isupper(c)){
+    value = ((c-'A')+rshift)%26 + 'A';
+    return value;
+  }
+  else if (islower(c)){
+    value = ((c-'a')+rshift)%26+'a';
+    return value;
+  }
+  else{
+   return c;}
+}
+
+// Caesar cipher encryption
+std::string encryptCaesar(std::string plaintext, int rshift){
+  std::string text = "";
+  for (int i = 0; i < plaintext.length();i++){
+   text += shiftChar(plaintext[i], rshift);
+  }
+  return text;
+}
